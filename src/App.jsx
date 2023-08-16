6d61ca51112caaa21457ea059c3dd90896a29244
DhanrajGedam <dhanarajdhanugedam6@gmail.com>
2023-08-16 22:17:50 +0530

    Vite react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hero from './Components/Hero';
+import Navigation from './Components/Navigation';
+const App = () =>{
+  return(
+    <div>
+      <Navigation />  
+      <Hero />
+    </div>
+  )
+}
+export default App;
